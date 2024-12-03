ed8bcd6f69e490dde781963fb38e61951a839156
Ludovico Piero <lewdovico@gnuweeb.org>
2024-12-03 11:46:22 +0900

    ArchiSteamFarm: 6.0.8.7 -> 6.1.0.2
    Changelog: https://github.com/JustArchiNET/ArchiSteamFarm/releases/tag/6.1.0.2
    
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- pkgs/applications/misc/ArchiSteamFarm/web-ui/default.nix
+++ pkgs/applications/misc/ArchiSteamFarm/web-ui/default.nix
@@ -2,7 +2,7 @@
 
 buildNpmPackage rec {
   pname = "asf-ui";
-  version = "7c5eb33a79736ca57acff776af4bd88816773247";
+  version = "5a3591d51abcc4575c0a5a002688acc481586db0";
 
   src = fetchFromGitHub {
     owner = "JustArchiNET";
@@ -10,10 +10,10 @@ buildNpmPackage rec {
     # updated by the update script
     # this is always the commit that should be used with asf-ui from the latest asf version
     rev = version;
-    hash = "sha256-oOeXyhDnW2jeKGssxzgMIQElkH1uT5IALiXwxUg1ETQ=";
+    hash = "sha256-ChJdE1Ry1UePMd6FI1SEcYGxxyuu3ihh9OxmzmJcImc=";
   };
 
-  npmDepsHash = "sha256-nofLj2PaL+A/M6F6vteh4SmONmsQ5kLPOZ/jxokubgU=";
+  npmDepsHash = "sha256-sskML9fuFltmeg9qohG3044gcYsfML6ysN8glz4ChCo=";
 
   installPhase = ''
     runHook preInstall
